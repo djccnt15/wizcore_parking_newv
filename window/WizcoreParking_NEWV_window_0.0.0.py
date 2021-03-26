a1b469e450cf169e5c9184e7dafa656f85f1889a
djccnt15 <53306911+djccnt15@users.noreply.github.com>
2021-03-26 16:32:32 +0900

    Add files via upload

--- window/WizcoreParking_NEWV_window_0.0.0.py
+++ window/WizcoreParking_NEWV_window_0.0.0.py
@@ -1,43 +1,53 @@
-import os, sys
+import os, sys,time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import schedule
 
-# declaring var
-url = "http://220.75.173.245/"
-wiz_id = "A101101"
-wiz_pw = "Wizcore!0"
-car_num = "87로6770"
+def job():
+    # declaring var
+    url = "http://220.75.173.245/"
+    wiz_id = "A101101"
+    wiz_pw = "Wizcore!0"
+    car_num = "87로6770"
 
-# loading chrome web driver
-if getattr(sys, 'frozen', False):
-    chromedriver_path = os.path.join(sys._MEIPASS, "chromedriver.exe")
-    driver = webdriver.Chrome(chromedriver_path)
-else:
-    driver = webdriver.Chrome()
+    # loading chrome web driver
+    if getattr(sys, 'frozen', False):
+        chromedriver_path = os.path.join(sys._MEIPASS, "chromedriver.exe")
+        driver = webdriver.Chrome(chromedriver_path)
+    else:
+        driver = webdriver.Chrome()
 
-driver.implicitly_wait(time_to_wait=5)
+    driver.implicitly_wait(time_to_wait=5)
 
-# url loading
-driver.get(url)
+    # url loading
+    driver.get(url)
 
-# login
-driver.find_element_by_id("inputAccount").send_keys(wiz_id)
-driver.find_element_by_id("inputPassword").send_keys(wiz_pw)
-driver.find_element_by_class_name("btn.btn-lg.btn-primary.btn-block.btn-signin").click()
+    # login
+    driver.find_element_by_id("inputAccount").send_keys(wiz_id)
+    driver.find_element_by_id("inputPassword").send_keys(wiz_pw)
+    driver.find_element_by_class_name("btn.btn-lg.btn-primary.btn-block.btn-signin").click()
 
-# search car
-search_box_car = driver.find_element_by_id("ip_car")
-search_box_car.send_keys(car_num)
-search_box_car.send_keys(Keys.RETURN)
+    # search car
+    search_box_car = driver.find_element_by_id("ip_car")
+    search_box_car.send_keys(car_num)
+    search_box_car.send_keys(Keys.RETURN)
 
-# select car
-driver.find_element_by_xpath('//*[@id="carsearch_table"]//tbody//tr//td[normalize-space()="%s"]' % car_num).click()
+    # select car
+    driver.find_element_by_xpath('//*[@id="carsearch_table"]//tbody//tr//td[normalize-space()="%s"]' % car_num).click()
 
-# discount parking
-driver.find_element_by_xpath('//*[@id="dc_items"]/label[3]/input').click()
-driver.find_element_by_id("DC_Active").click()
-driver.find_element_by_xpath('//*[@id="dc_items"]/label[2]/input').click()
-driver.find_element_by_id("DC_Active").click()
+    # discount parking
+    driver.find_element_by_xpath('//*[@id="dc_items"]/label[3]/input').click()
+    driver.find_element_by_id("DC_Active").click()
+    driver.find_element_by_xpath('//*[@id="dc_items"]/label[2]/input').click()
+    driver.find_element_by_id("DC_Active").click()
 
-# quit web driver
-driver.quit()
+    # quit web driver
+    driver.quit()
+
+# schedule
+schedule.every().friday.at("06:30").do(job)
+
+# run every 10 seconds
+while True:
+    schedule.run_pending()
+    time.sleep(1)
