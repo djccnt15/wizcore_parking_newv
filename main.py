375b633f6cf6350091536fd71c2981e8938de9f1
djccnt15 <djccnt15@gmail.com>
2023-12-07 00:07:13 +0900

    update main. upgrade dependency version version up selenium dependency from 3.1 to 4.1

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 import json
-import platform
 from argparse import ArgumentParser
 from pathlib import Path
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 
 from src.common.enums import state
+from src.common.error import LoginError
 from src.utils.log import logger
 
 
-def loading_webdriver(driver_path: Path | str, headless: int):
+def loading_webdriver(headless: int):
     options = webdriver.ChromeOptions()
     if headless:
         options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(executable_path=str(driver_path), options=options)
+
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(time_to_wait=5)
     return driver
 
 
 def login(driver: WebDriver, auth: dict):
-    driver.find_element_by_id("inputAccount").send_keys(auth["id"])
-    driver.find_element_by_id("inputPassword").send_keys(auth["pw"])
-    driver.find_element_by_class_name(
-        "btn.btn-lg.btn-primary.btn-block.btn-signin"
+    driver.find_element(by=By.ID, value="inputAccount").send_keys(auth["id"])
+    driver.find_element(by=By.ID, value="inputPassword").send_keys(auth["pw"])
+    driver.find_element(
+        by=By.CLASS_NAME, value="btn.btn-lg.btn-primary.btn-block.btn-signin"
     ).click()
     return driver
 
 
+def login_check(driver: WebDriver):
+    profile_name_card = driver.find_element(by=By.ID, value="profile-name")
+    if profile_name_card:
+        return profile_name_card.text
+
+
 def search_car(driver: WebDriver, car_num):
-    search_box_car = driver.find_element_by_id("ip_car")
+    search_box_car = driver.find_element(by=By.ID, value="ip_car")
     search_box_car.send_keys(car_num)
     search_box_car.send_keys(Keys.RETURN)
     return driver
 
 
 def select_car(driver: WebDriver, car_num: str):
-    driver.find_element_by_xpath(
-        '//*[@id="carsearch_table"]//tbody//tr//td[normalize-space()="%s"]' % (car_num)
+    driver.find_element(
+        by=By.XPATH,
+        value=f'//*[@id="carsearch_table"]//tbody//tr//td[normalize-space()="{car_num}"]',
     ).click()
     return driver
 
@@ -50,24 +60,22 @@ def select_car(driver: WebDriver, car_num: str):
 def discount(driver: WebDriver, discount_type: int):
     """discount type: 1 == 10min, 2 == 30min, 3 == 1hour"""
     try:
-        driver.find_element_by_xpath(
-            '//*[@id="dc_items"]/label[%s]/input' % (discount_type)
+        driver.find_element(
+            by=By.XPATH, value=f'//*[@id="dc_items"]/label[{discount_type}]/input'
         ).click()
-        driver.find_element_by_id("DC_Active").click()
-        alert = WebDriverWait(driver, timeout=2).until(EC.alert_is_present())
-    except Exception:
-        if discount_type == 3:
-            logger.info(state.DiscountResult.SUCCESS % "1h")
-        elif discount_type == 2:
-            logger.info(state.DiscountResult.SUCCESS % "30m")
-        elif discount_type == 1:
-            logger.info(state.DiscountResult.SUCCESS % "10m")
-    else:
-        try:
-            raise Exception
-        except Exception as e:
-            ERROR_MSG = f"{e}\n{alert.text}"
-            logger.exception(state.DiscountResult.ERROR % ERROR_MSG)
+        driver.find_element(by=By.ID, value="DC_Active").click()
+    except NoSuchElementException as e:
+        ERROR_MSG = f"{state.DiscountResult.ERROR}\n{e}"
+        logger.exception(ERROR_MSG)
+        raise NoSuchElementException
+
+    if discount_type == 3:
+        logger.info(state.DiscountResult.SUCCESS % "1h")
+    elif discount_type == 2:
+        logger.info(state.DiscountResult.SUCCESS % "30m")
+    elif discount_type == 1:
+        logger.info(state.DiscountResult.SUCCESS % "10m")
+
     return driver
 
 
@@ -83,42 +91,40 @@ def main(headless: int, log_level: int, info_path: Path):
 
     # declaring var
     with open(info_path, encoding="utf-8") as f:
-        info: dict = json.load(f)
-
-    driver_path = "chromedriver"
-    if platform.system() == "Windows":
-        driver_path = f"{driver_path}.exe"
+        config: dict = json.load(f)
 
     # loading webdriver
     try:
-        driver = loading_webdriver(driver_path=driver_path, headless=headless)
+        driver = loading_webdriver(headless=headless)
     except Exception as e:
         ERROR_MSG = f"{state.WebDriverState.ERROR}\n{e}"
         logger.exception(ERROR_MSG)
         raise Exception
     logger.info(state.WebDriverState.SUCCESS)
 
-    # url loading
+    # web page loading
     try:
-        driver.get(info["url"])
+        driver.get(config["url"])
     except Exception:
         logger.error(state.UrlState.ERROR)
         raise Exception
     logger.info(state.UrlState.SUCCESS % driver.current_url)
 
     # login
+    driver = login(driver=driver, auth=config["auth"])
     try:
-        driver = login(driver=driver, auth=info["auth"])
-    except Exception:
-        logger.error(state.LoginState.ERROR)
-        raise Exception
-    logger.info(state.LoginState.SUCCESS % driver.current_url)
+        login_res = login_check(driver)
+    except NoSuchElementException:
+        logger.info(state.LoginState.SUCCESS % driver.current_url)
+    else:
+        logger.error(state.LoginState.ERROR % login_res)
+        raise LoginError
 
     # search car
     try:
-        driver = search_car(driver=driver, car_num=info["car_num"])
+        driver = search_car(driver=driver, car_num=config["car_num"])
         alert = WebDriverWait(driver, timeout=2).until(EC.alert_is_present())
-    except Exception:
+    except TimeoutException:
         logger.info(state.SearchCarState.SUCCESS)
     else:
         logger.error(state.SearchCarState.ERROR % alert.text)
@@ -126,8 +132,8 @@ def main(headless: int, log_level: int, info_path: Path):
 
     # select car
     try:
-        driver = select_car(driver=driver, car_num=info["car_num"])
-    except Exception:
+        driver = select_car(driver=driver, car_num=config["car_num"])
+    except NoSuchElementException:
         logger.error(state.SelectCarState.ERROR)
         raise Exception
     logger.info(state.SelectCarState.SUCCESS)
@@ -141,9 +147,9 @@ def main(headless: int, log_level: int, info_path: Path):
 
     # search car
     try:
-        driver = search_car(driver=driver, car_num=info["car_num"])
+        driver = search_car(driver=driver, car_num=config["car_num"])
         alert = WebDriverWait(driver, timeout=2).until(EC.alert_is_present())
-    except Exception:
+    except TimeoutException:
         logger.info(state.SearchCarState.SUCCESS)
     else:
         logger.error(state.SearchCarState.ERROR % alert.text)
@@ -151,8 +157,8 @@ def main(headless: int, log_level: int, info_path: Path):
 
     # select car
     try:
-        driver = select_car(driver=driver, car_num=info["car_num"])
-    except Exception:
+        driver = select_car(driver=driver, car_num=config["car_num"])
+    except NoSuchElementException:
         logger.error(state.SelectCarState.ERROR)
     logger.info(state.SelectCarState.SUCCESS)
 
