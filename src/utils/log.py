ba5af423c302fd465fd94781cfa9bfef389dde1a
djccnt15 <djccnt15@gmail.com>
2023-12-10 00:13:52 +0900

    update log module add StreamHandler to log handlers

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,29 +1,36 @@
 import logging
+from logging import StreamHandler
 from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
-# set directory
+# set log directory
 log_dir = Path("logs")
 try:
     log_dir.mkdir()
 except FileExistsError:
     ...
-LOG_FILENAME = log_dir / "main.log"
 
 # set log format
 formatter = logging.Formatter(
     "%(asctime)s - %(levelname)s - [%(filename)s:%(module)s:%(name)s:%(lineno)d] %(message)s"
 )
 
-# create TimedRotatingFileHandler and setting it
+# create TimedRotatingFileHandler
 file_handler = TimedRotatingFileHandler(
-    filename=LOG_FILENAME, when="midnight", interval=1, encoding="utf-8"
-)  # rotate every midnight
-file_handler.suffix = "%Y%m%d.log"
+    filename=log_dir / "log.log",
+    when="midnight",  # rotate every midnight
+    backupCount=3,  # define number of log files, set 0 to save infinity log files
+    encoding="utf-8",
+)
 file_handler.setFormatter(formatter)
 
-# create Logger instance
-logger = logging.getLogger("main_logger")
+# create StreamHandler
+stream_handler = StreamHandler()
+stream_handler.setFormatter(formatter)
 
-# add file handler
+# create Logger instance
+logger = logging.getLogger("logger")
+
+# add log handler to logger
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
